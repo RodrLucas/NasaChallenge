72c2f113ebe4ebd6d2825aacf06b9e7f535ded88
RodrLucas <lucasrodriguessantos0@gmail.com>
2022-09-20 13:20:05 -0300

    refacor: using reduce instead foreach

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Folder } from "./uses/folder";
 import path from "path";
+import { IFinalPositionProps } from "./interfaces/FinalPosition";
 
 const OPTIONS = { encoding: "utf-8", withFileTypes: false };
 const COORDS_PATH = path.join(__dirname, "COORDS");
@@ -7,48 +8,47 @@ const COORDS_PATH = path.join(__dirname, "COORDS");
 const dataFolder = Folder.readDir({ COORDS_PATH, OPTIONS });
 const dataFiles = Folder.readFile({ COORDS_PATH, OPTIONS }, dataFolder);
 
-interface IFinalPositionProps {
-  x: number;
-  y: number;
-  direction: string;
-}
-
 dataFiles.forEach((probes) => {
-  Object.entries(probes).forEach((probe) => {
-    const moviments = probe[1].directions.split(",");
-    const initialPosition = probe[1].initialPosition.split(",");
-    const finalPosition: IFinalPositionProps = {
-      x: +initialPosition[0],
-      y: +initialPosition[1],
-      direction: initialPosition[2],
-    };
+  Object.entries(probes).forEach((probe: string[] | object) => {
+    const moviments: string[] = probe[1].directions.split(",");
+    const initialPosition: string[] = probe[1].initialPosition.split(",");
 
-    moviments.forEach((moviment: string) => {
-      const left = {
-        N: () => (finalPosition.direction = "W"),
-        W: () => (finalPosition.direction = "S"),
-        S: () => (finalPosition.direction = "E"),
-        E: () => (finalPosition.direction = "N"),
-      };
+    const results = moviments.reduce(
+      ({ x, y, direction }: IFinalPositionProps, moviment) => {
+        const left = {
+          N: () => (direction = "W"),
+          W: () => (direction = "S"),
+          S: () => (direction = "E"),
+          E: () => (direction = "N"),
+        };
 
-      const right = {
-        N: () => (finalPosition.direction = "E"),
-        E: () => (finalPosition.direction = "S"),
-        S: () => (finalPosition.direction = "W"),
-        W: () => (finalPosition.direction = "N"),
-      };
+        const right = {
+          N: () => (direction = "E"),
+          E: () => (direction = "S"),
+          S: () => (direction = "W"),
+          W: () => (direction = "N"),
+        };
 
-      const move = {
-        N: () => finalPosition.y++,
-        E: () => finalPosition.x++,
-        S: () => finalPosition.y--,
-        W: () => finalPosition.x--,
-      };
+        const move = {
+          N: () => y++,
+          E: () => x++,
+          S: () => y--,
+          W: () => x--,
+        };
 
-      if (moviment === "L") left[finalPosition.direction]();
-      if (moviment === "R") right[finalPosition.direction]();
-      if (moviment === "M") move[finalPosition.direction]();
-    });
-    console.log("finalPosition", finalPosition);
+        if (moviment === "L") left[direction]();
+        if (moviment === "R") right[direction]();
+        if (moviment === "M") move[direction]();
+
+        return { x, y, direction };
+      },
+      {
+        x: +initialPosition[0],
+        y: +initialPosition[1],
+        direction: initialPosition[2],
+      }
+    );
+
+    console.log('results', results)
   });
 });
---
+++ src/interfaces/FinalPosition.ts
@@ -0,0 +1,5 @@
+export interface IFinalPositionProps {
+    x: number;
+    y: number;
+    direction: string;
+  }
