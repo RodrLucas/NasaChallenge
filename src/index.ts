ff73c51c57011bb95f7387070b6c717a39b0e924
RodrLucas <lucasrodriguessantos0@gmail.com>
2022-09-20 11:53:45 -0300

    feat: result of finalPosition print on console

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,60 @@
 import { Folder } from "./uses/folder";
-import path from 'path'
+import path from "path";
 
-const OPTIONS = { encoding: "utf-8", withFileTypes: false }
+const OPTIONS = { encoding: "utf-8", withFileTypes: false };
 const COORDS_PATH = path.join(__dirname, "COORDS");
-const EXIT_FILES_PATH = path.join(__dirname, "EXIT_COORDS")
 
-Folder.createDir(EXIT_FILES_PATH)
-const dataFolder = Folder.readDir({COORDS_PATH, OPTIONS})
-const dataFiles = Folder.readFile({COORDS_PATH, OPTIONS}, dataFolder)
+const dataFolder = Folder.readDir({ COORDS_PATH, OPTIONS });
+const dataFiles = Folder.readFile({ COORDS_PATH, OPTIONS }, dataFolder);
 
-console.log(dataFiles)
+dataFiles.forEach(
+  (probes) => {
+    Object.entries(probes).forEach((probe) => {
+      const moviments = probe[1].directions.split(",");
+      const initialPosition = probe[1].initialPosition.split(",");
+      const finalPosition = {
+        x: +initialPosition[0],
+        y: +initialPosition[1],
+        direction: initialPosition[2],
+      };
 
+      moviments.forEach((moviment) => {
+        if (moviment === "L") {
+          if (finalPosition.direction === "N") {
+            finalPosition.direction = "W";
+          } else if (finalPosition.direction === "W") {
+            finalPosition.direction = "S";
+          } else if (finalPosition.direction === "S") {
+            finalPosition.direction = "E";
+          } else if (finalPosition.direction === "E") {
+            finalPosition.direction = "N";
+          }
+        }
+
+        if (moviment === "R") {
+          if (finalPosition.direction === "N") {
+            finalPosition.direction = "E";
+          } else if (finalPosition.direction === "E") {
+            finalPosition.direction = "S";
+          } else if (finalPosition.direction === "S") {
+            finalPosition.direction = "W";
+          } else if (finalPosition.direction === "W") {
+            finalPosition.direction = "N";
+          }
+        }
+
+        if (moviment === "M") {
+          if (finalPosition.direction === "N") {
+            finalPosition.y++;
+          } else if (finalPosition.direction === "E") {
+            finalPosition.x++;
+          } else if (finalPosition.direction === "S") {
+            finalPosition.y--;
+          } else if (finalPosition.direction === "W") {
+            finalPosition.x--;
+          }
+        }
+      });
+      console.log('finalPosition', finalPosition)
+    });
+  });
--- src/uses/folder.ts
+++ src/uses/folder.ts
@@ -1,16 +1,6 @@
 import fs from "fs";
 
 export class Folder {
-  static createDir(EXIT_FILES_PATH) {
-    fs.mkdir(EXIT_FILES_PATH, (err) => {
-      if (err) console.error(err);
-      else console.log("Directory created successfully!");
-    });
-    return {
-      EXIT_FILES_PATH,
-    };
-  }
-
   static readDir({ COORDS_PATH, OPTIONS }) {
     const dataFolder = fs.readdirSync(COORDS_PATH, OPTIONS);
     return dataFolder;
