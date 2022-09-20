577628116aa70ecc91ccf62637ceb9635aec8ef2
RodrLucas <lucasrodriguessantos0@gmail.com>
2022-09-20 12:54:55 -0300

    refactor: using ObjectLiteral instead ifs

--- src/index.ts
+++ src/index.ts
@@ -7,54 +7,48 @@ const COORDS_PATH = path.join(__dirname, "COORDS");
 const dataFolder = Folder.readDir({ COORDS_PATH, OPTIONS });
 const dataFiles = Folder.readFile({ COORDS_PATH, OPTIONS }, dataFolder);
 
-dataFiles.forEach(
-  (probes) => {
-    Object.entries(probes).forEach((probe) => {
-      const moviments = probe[1].directions.split(",");
-      const initialPosition = probe[1].initialPosition.split(",");
-      const finalPosition = {
-        x: +initialPosition[0],
-        y: +initialPosition[1],
-        direction: initialPosition[2],
+interface IFinalPositionProps {
+  x: number;
+  y: number;
+  direction: string;
+}
+
+dataFiles.forEach((probes) => {
+  Object.entries(probes).forEach((probe) => {
+    const moviments = probe[1].directions.split(",");
+    const initialPosition = probe[1].initialPosition.split(",");
+    const finalPosition: IFinalPositionProps = {
+      x: +initialPosition[0],
+      y: +initialPosition[1],
+      direction: initialPosition[2],
+    };
+
+    moviments.forEach((moviment: string) => {
+      const left = {
+        N: () => (finalPosition.direction = "W"),
+        W: () => (finalPosition.direction = "S"),
+        S: () => (finalPosition.direction = "E"),
+        E: () => (finalPosition.direction = "N"),
       };
 
-      moviments.forEach((moviment) => {
-        if (moviment === "L") {
-          if (finalPosition.direction === "N") {
-            finalPosition.direction = "W";
-          } else if (finalPosition.direction === "W") {
-            finalPosition.direction = "S";
-          } else if (finalPosition.direction === "S") {
-            finalPosition.direction = "E";
-          } else if (finalPosition.direction === "E") {
-            finalPosition.direction = "N";
-          }
-        }
+      const right = {
+        N: () => (finalPosition.direction = "E"),
+        E: () => (finalPosition.direction = "S"),
+        S: () => (finalPosition.direction = "W"),
+        W: () => (finalPosition.direction = "N"),
+      };
 
-        if (moviment === "R") {
-          if (finalPosition.direction === "N") {
-            finalPosition.direction = "E";
-          } else if (finalPosition.direction === "E") {
-            finalPosition.direction = "S";
-          } else if (finalPosition.direction === "S") {
-            finalPosition.direction = "W";
-          } else if (finalPosition.direction === "W") {
-            finalPosition.direction = "N";
-          }
-        }
+      const move = {
+        N: () => finalPosition.y++,
+        E: () => finalPosition.x++,
+        S: () => finalPosition.y--,
+        W: () => finalPosition.x--,
+      };
 
-        if (moviment === "M") {
-          if (finalPosition.direction === "N") {
-            finalPosition.y++;
-          } else if (finalPosition.direction === "E") {
-            finalPosition.x++;
-          } else if (finalPosition.direction === "S") {
-            finalPosition.y--;
-          } else if (finalPosition.direction === "W") {
-            finalPosition.x--;
-          }
-        }
-      });
-      console.log('finalPosition', finalPosition)
+      if (moviment === "L") left[finalPosition.direction]();
+      if (moviment === "R") right[finalPosition.direction]();
+      if (moviment === "M") move[finalPosition.direction]();
     });
+    console.log("finalPosition", finalPosition);
   });
+});
